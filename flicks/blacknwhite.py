ff37dd5727237382b655f621239e051cd3a9490d
thesexguru <dj.esposito@icloud.com>
2021-04-26 22:32:33 -0500

    changing colors

--- flicks/blacknwhite.py
+++ flicks/blacknwhite.py
@@ -5,6 +5,6 @@ def black_and_white(input_image_path, output_image_path):
    color_image = Image.open(input_image_path)
    bw = color_image.convert('L')
    bw.save(output_image_path)
+
 if __name__ == '__main__':
-    black_and_white('screenshot2.jpg','bw3_screenshot.jpg')
-    
+    black_and_white('screenshot4.jpg','b_screenshot.jpg')
