0b896de6b0824e0cfcd4951f0fab30081d7e611b
thesexguru <dj.esposito@icloud.com>
2021-04-27 21:59:49 -0500

    new proto

--- flicks/prototype.py
+++ flicks/prototype.py
@@ -9,17 +9,32 @@ print("*")
 print("*")
 print("DRIPBOT IS LIVE")
 print("_________________________________________________________________________")
-call(["screencapture", "-R390,280,150,50", "test.jpg"])
-
 balance_list = []
-image = Image.open('test.jpg')
-new_image = image.resize((250, 50))
-new_image.save("test.png")
+num_loss = 0
 
+#pyautogui functions
+def chip():
+    pyautogui.click(x=691, y=887, clicks=1, button='left')
 
-#CAPTURES A SCREEN GRAB OF CURRENT BALANCE
-# Captures area of screen
-#CONVERTS IMAGE TO GRAYSCALE
+def player():
+    pyautogui.click(x=714, y=613, clicks=1, button='left')
+
+def deal():
+    pyautogui.click(x=1564, y=616, clicks=1, button='left')
+
+def re_bet():
+    pyautogui.click(x=1560, y=528, clicks=1, button='left')
+
+def re_bet2x():
+    pyautogui.click(x=1561, y=703, clicks=1, button='left')
+
+#image functions
+def screengrab():
+    call(["screencapture", "-R390,280,150,50", "test.jpg"])
+    image = Image.open('test.jpg')
+    new_image = image.resize((250, 50))
+    new_image.save("test.png")
+
 def black_and_white(input_image_path, output_image_path):
    color_image = Image.open(input_image_path)
    bw = color_image.convert('L')
@@ -43,6 +58,7 @@ def adjust_resolution():
     image_file = Image.open(image_path)
     image_file.save("test4.png", dpi=(500,500))
 
+#tesseract function
 def tess_run():
     os.system("tesseract test4.png tess 6")
     file_in = open('tess.txt', 'r')
@@ -53,12 +69,7 @@ def tess_run():
     z = balance_list.append(q)
     print(balance_list)
 
-
-#OCR ANALYZES SCREENGRAB AND PROVIDES A .TXT FILE INTERPRETATION IN THE SAME DIRECTORY
-
-#if __name__ == '__main__':
-i = 0
-while i < 4:
+def start_game():
 
     if __name__ == '__main__':
         x = black_and_white("test.png", "test1.png")
@@ -66,25 +77,6 @@ while i < 4:
         resize_image()
         adjust_resolution()
         tess_run()
-    i += 1
-
-
-
-
-
-#    black_and_white('screenshot2.jpg','bw3_screenshot.jpg'
-
-#re-bet coordinates: (1167,420)
-#re-betx2 coordinates: (1154,544)
-#pyautogui example syntax: pyautogui.click(x=1154, y=5444, clicks=1, interval=20, button='left')
-
-#EXAMPLE FOR LOOP
-#x = 0
-#while x < 10:
-#    pyautogui.click(x=1154, y=544, clicks=1, button='left')
-#    pyautogui.click(x=1154, y=544, clicks=1, button='left')
-#    pyautogui.click(interval = 7)
-#    x+=1
 
 print("_________________________________________________________________________")
 print("DRIPBOT OUT")
